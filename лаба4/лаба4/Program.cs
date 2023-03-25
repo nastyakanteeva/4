a0602d87011e2af2b53ea594fde7f31b1c3709e3
nastyakanteeva <117898518+nastyakanteeva@users.noreply.github.com>
2023-03-25 10:25:20 +0300

    Add files via upload

---
+++ лаба4/лаба4/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    const int maxArrayLength = 1000;
+    private static void Main(string[] args)
+    {
+        // заполнение массива 
+        var counters = new int[maxArrayLength];
+        // Ввод данных
+        for (int N = int.Parse(Console.ReadLine()); N > 0; N--)
+            counters[int.Parse(Console.ReadLine())]++;
+        // Подсчет пар и вывод
+        for (int i = 0; i < counters.Length; i++)
+        {
+            if (counters[i] != 0)
+                Console.WriteLine($"{i} {counters[i]}");
+        }
+    }
+}
